3d38d222a69d78a630b2913cb4b264db045ea10f
StefenGPD <decena.stefengenoa@gmail.com>
2023-01-30 10:29:45 +0800

    Updated introduction at index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <b>Stefen Genoa Decena.</b> I'm a dev intern at TwistResources. Nice to meet you!</p>
+        <p>Hello, I'm <b>Stefen Genoa Decena.</b> I'm a 4th year BS in Computer Science student, and currently I'm a dev intern at TwistResources. Nice to meet you!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
