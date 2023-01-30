1cbae44b5219d5155bd9343229dabb8e8c90ba35
StefenGPD <decena.stefengenoa@gmail.com>
2023-01-30 09:52:11 +0800

    Finished the introduction part

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,15 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
@@ -10,6 +17,6 @@ export default function FirstPost() {
       <h2>
         <Link href="second-post">Testing Second page</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
